56da1269cb6b1f59bad30dfeabbd249d56bba0f9
MP <MP@LINBLR3WX82Z2.internal.spherasolutions.com>
2020-12-02 11:27:12 +0530

    login

---
+++ Test Suites/Orange login.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Orange login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c491032e-c687-499c-aece-170f9abe4818</testSuiteGuid>
+   <testCaseLink>
+      <guid>126d908c-eaa6-41c6-9f82-ea87eb2f58fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Orange login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>1e26353a-99bc-4a3c-8198-aba211b0fb87</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/data1</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>1e26353a-99bc-4a3c-8198-aba211b0fb87</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>6224b540-bbf0-4a9d-ac41-ccb4cd030aea</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>1e26353a-99bc-4a3c-8198-aba211b0fb87</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>5a6f3a58-6272-42b1-8c38-bf1a2dedea6b</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
